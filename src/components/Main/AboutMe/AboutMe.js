180137107df607df1975ee39c77cd4720d6acc32
alexsaushkin <thirdyou@yandex.ru>
2023-07-03 10:49:48 +0300

    add: add about-me

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -6,34 +6,32 @@ import myPhoto from '../../../images/my-photo.jpg';
 export default function AboutMe() {
   return (
     <section className='about-me' id='about-me'>
-      <div className='about-me__content'>
-        <BlockTitle text='Студент' />
-        <article className='about-me__block'>
-          <div className='about-me__information'>
-            <h3 className='about-me__title'>Алексей</h3>
-            <p className='about-me__description'>Фронтенд-разработчик, 25 лет</p>
-            <p className='about-me__text'>
-              Я родился в Тольятти, живу в Санкт-Петербурге, закончил бакалавриат в СПбГУ,
-              магистратуру в НИУ ВШЭ по направлению юриспруденция.
-              С 2022 года начал учиться фронтенд-разработке.
-            </p>
-            <a
-              className='about-me__link'
-              href='https://github.com/alexsaushkin'
-              target='_blank'
-              rel='noreferrer'
-            >
-              Github
-            </a>
-          </div>
-          <img
-            className='about-me__photo'
-            src={myPhoto}
-            alt='Фото автора'
-          />
-        </article>
-        <Portfolio />
-      </div>
+      <BlockTitle title='Студент'/>
+      <article className='about-me__block'>
+        <div className='about-me__information'>
+          <h3 className='about-me__title'>Алексей</h3>
+          <p className='about-me__description'>Фронтенд-разработчик, 25 лет</p>
+          <p className='about-me__text'>
+            Я родился в Тольятти, живу в Санкт-Петербурге, закончил бакалавриат в СПбГУ,
+            магистратуру в НИУ ВШЭ по направлению юриспруденция.
+            С 2022 года начал учиться фронтенд-разработке.
+          </p>
+          <a
+            className='about-me__link'
+            href='https://github.com/alexsaushkin'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Github
+          </a>
+        </div>
+        <img
+          className='about-me__photo'
+          src={myPhoto}
+          alt='Фото автора'
+        />
+      </article>
+      <Portfolio/>
     </section>
   );
 }
