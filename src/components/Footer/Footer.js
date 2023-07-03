f59d4a902a39795550c98d69f0744cd7e77da463
alexsaushkin <thirdyou@yandex.ru>
2023-07-03 11:14:53 +0300

    add: add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <h1>Footer</h1>
+    <footer className='footer'>
+      <h4 className='footer__subtitle'>
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </h4>
+      <div className='footer__content'>
+        <p className='footer__copyright'>&copy;&nbsp;{(new Date().getFullYear())}</p>
+        <ul className='footer_links'>
+          <li>
+            <a
+              className='footer__link'
+              href='http://practicum.yandex.ru/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li>
+            <a
+              className='footer__link'
+              href='https://github.com/alexsaushkin'
+              target='_blank'
+              rel='noreferrer'
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
 }
