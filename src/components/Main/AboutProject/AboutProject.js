93a5e3a8271b596767ca779839b355e6f9d10df9
alexsaushkin <thirdyou@yandex.ru>
2023-07-03 10:12:29 +0300

    add: add techs

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -4,47 +4,45 @@ import './AboutProject.css';
 export default function AboutProject() {
   return (
     <section className='about-project' id='about-project'>
-      <div className='about-project__content'>
-        <BlockTitle title='О проекте'/>
-        <ul className='about-project__block'>
-          <li className='about-project__block-item'>
-            <h3 className='about-project__subtitle'>
-              Дипломный проект включал 5 этапов
-            </h3>
-            <p className='about-project__description'>
-              Составление плана, работу над бэкендом, вёрстку,
-              добавление функциональности и финальные доработки.
-            </p>
-          </li>
-          <li className='about-project__block-item'>
-            <h3 className='about-project__subtitle'>
-              На выполнение диплома ушло 5 недель
-            </h3>
-            <p className='about-project__description'>
-              У каждого этапа был мягкий и жёсткий дедлайн, которые
-              нужно было соблюдать, чтобы успешно защититься.
-            </p>
-          </li>
-        </ul>
-        <ul className='about-project__timeline'>
-          <li className='about-project__timeline-item'>
-            <h4 className='about-project__timeline-time about-project__timeline-time_color_green'>
-              1 неделя
-            </h4>
-            <p className='about-project__timeline-text'>
-              Back-end
-            </p>
-          </li>
-          <li className='about-project__timeline-item about-project__timeline-item_wide'>
-            <h4 className='about-project__timeline-time'>
-              4 неделя
-            </h4>
-            <p className='about-project__timeline-text'>
-              Front-end
-            </p>
-          </li>
-        </ul>
-      </div>
+      <BlockTitle title='О проекте'/>
+      <ul className='about-project__block'>
+        <li className='about-project__block-item'>
+          <h3 className='about-project__subtitle'>
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className='about-project__description'>
+            Составление плана, работу над бэкендом, вёрстку,
+            добавление функциональности и финальные доработки.
+          </p>
+        </li>
+        <li className='about-project__block-item'>
+          <h3 className='about-project__subtitle'>
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className='about-project__description'>
+            У каждого этапа был мягкий и жёсткий дедлайн, которые
+            нужно было соблюдать, чтобы успешно защититься.
+          </p>
+        </li>
+      </ul>
+      <ul className='about-project__timeline'>
+        <li className='about-project__timeline-item'>
+          <h4 className='about-project__timeline-time about-project__timeline-time_color_green'>
+            1 неделя
+          </h4>
+          <p className='about-project__timeline-text'>
+            Back-end
+          </p>
+        </li>
+        <li className='about-project__timeline-item about-project__timeline-item_wide'>
+          <h4 className='about-project__timeline-time'>
+            4 неделя
+          </h4>
+          <p className='about-project__timeline-text'>
+            Front-end
+          </p>
+        </li>
+      </ul>
     </section>
   );
 }
--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -4,25 +4,23 @@ import BlockTitle from "../BlockTitle/BlockTitle";
 export default function Techs() {
   return (
     <section className='techs' id='techs'>
-      <div className='techs__content'>
-        <BlockTitle title='Технологии'/>
-        <h3 className='techs__title'>
-          7 технологий
-        </h3>
-        <p className='techs__description'>
-          На курсе веб-разработки мы освоили технологии,
-          которые применили в дипломном проекте.
-        </p>
-        <ul className='techs__skills'>
-          <li className='techs__skill'>HTML</li>
-          <li className='techs__skill'>CSS</li>
-          <li className='techs__skill'>JS</li>
-          <li className='techs__skill'>React</li>
-          <li className='techs__skill'>Git</li>
-          <li className='techs__skill'>Express.js</li>
-          <li className='techs__skill'>mongoDB</li>
-        </ul>
-      </div>
+      <BlockTitle title='Технологии'/>
+      <h3 className='techs__title'>
+        7 технологий
+      </h3>
+      <p className='techs__description'>
+        На курсе веб-разработки мы освоили технологии,
+        которые применили в дипломном проекте.
+      </p>
+      <ul className='techs__skills'>
+        <li className='techs__skill'>HTML</li>
+        <li className='techs__skill'>CSS</li>
+        <li className='techs__skill'>JS</li>
+        <li className='techs__skill'>React</li>
+        <li className='techs__skill'>Git</li>
+        <li className='techs__skill'>Express.js</li>
+        <li className='techs__skill'>mongoDB</li>
+      </ul>
     </section>
   )
 }
