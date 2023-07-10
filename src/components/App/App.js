6f9348acae89ccf6dfdcb2fc07d09407d822c920
alexsaushkin <thirdyou@yandex.ru>
2023-07-10 06:51:34 +0300

    update: update profile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,52 +1,55 @@
-import React, {useState, useEffect} from 'react';
-import {Routes, Route, useNavigate, Navigate} from 'react-router-dom';
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
-import Main from '../Main/Main';
-import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies';
-import Profile from '../Profile/Profile';
-import SignIn from '../SignIn/SignIn';
-import SignUp from '../SignUp/SignUp';
-import NotFound from '../NotFound/NotFound';
-import moviesApi from '../../utils/MoviesApi';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Main from "../Main/Main";
+import Movies from "../Movies/Movies";
+import SavedMovies from "../SavedMovies/SavedMovies";
+import Profile from "../Profile/Profile";
+import SignIn from "../SignIn/SignIn";
+import SignUp from "../SignUp/SignUp";
+import NotFound from "../NotFound/NotFound";
+import moviesApi from "../../utils/MoviesApi";
+import CurrentUserContext from '../../contexts/CurrentUserContext';
+import "./App.css";
 
 function App() {
   const navigate = useNavigate();
   const [signedIn, setSignedIn] = useState(false);
-  const [movies, setMovies] = React.useState([]);
+  const [movies, setMovies] = useState([]);
+  const [currentUser, setCurrentUser] = useState({name: 'Алексей', email: 'thirdyou@yandex.ru'});
 
   function handleSignUp() {
-    navigate('/sign-in', {replace: true});
+    navigate("/sign-in", { replace: true });
   }
 
   function handleSignIn() {
     setSignedIn(true);
-    navigate('/movies', {replace: true});
+    navigate("/movies", { replace: true });
   }
 
   function handleSignOut() {
-    navigate('/', {replace: true});
+    navigate("/", { replace: true });
     setSignedIn(false);
   }
 
   function onSave(card) {
-    console.log(`${card} card saved`)
+    console.log(`${card} card saved`);
   }
 
   function onDelete(card) {
-    console.log(`${card} card deleted`)
+    console.log(`${card} card deleted`);
   }
 
   function getData() {
-    moviesApi.getMovies()
+    moviesApi
+      .getMovies()
       .then((moviesData) => {
         setMovies(moviesData);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
-      })
+      });
   }
 
   useEffect(() => {
@@ -54,26 +57,43 @@ function App() {
   }, [signedIn]);
 
   return (
-    <div className='app'>
-      <Header
-        signedIn={signedIn}
-      />
-      <Routes>
-        <Route path='/' element={<Main/>}/>
-        <Route path='/movies' element={<Movies movies={movies.slice(0, 12)} onSave={onSave}/>}/>
-        <Route path='/saved-movies' element={<SavedMovies movies={movies.slice(0, 3)} onDelete={onDelete}/>}/>
-        <Route path='/profile' element={<Profile handleSignOut={handleSignOut}/>}/>
-        <Route path='/signin' element={<SignIn handleSignIn={handleSignIn}/>}/>
-        <Route path='/signup' element={<SignUp handleSignUp={handleSignUp}/>}/>
-        <Route path='/*' element={<NotFound/>}/>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="app">
+        <Header signedIn={signedIn} />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route
+            path="/movies"
+            element={<Movies movies={movies.slice(0, 12)} onSave={onSave} />}
+          />
+          <Route
+            path="/saved-movies"
+            element={
+              <SavedMovies movies={movies.slice(0, 3)} onDelete={onDelete} />
+            }
+          />
+          <Route
+            path="/profile"
+            element={<Profile handleSignOut={handleSignOut} />}
+          />
+          <Route
+            path="/signin"
+            element={<SignIn handleSignIn={handleSignIn} />}
+          />
+          <Route
+            path="/signup"
+            element={<SignUp handleSignUp={handleSignUp} />}
+          />
+          <Route path="/*" element={<NotFound />} />
 
-        <Route path='/'
-               element={signedIn ? <Navigate to='/'/> : <Navigate to='/signin'/>}
-        />
-
-      </Routes>
-      <Footer/>
-    </div>
+          <Route
+            path="/"
+            element={signedIn ? <Navigate to="/" /> : <Navigate to="/signin" />}
+          />
+        </Routes>
+        <Footer />
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,45 @@
+import React, {useContext, useEffect, useState} from "react";
+import CurrentUserContext from '../../contexts/CurrentUserContext'
 import './Profile.css';
 
-export default function Profile() {
+export default function Profile({onUpdateUser}) {
+  const currentUser = useContext(CurrentUserContext);
+  const [name, setName] = useState(currentUser.name);
+  const [email, setEmail] = useState(currentUser.email);
+  const [isEdit, setIsEdit] = useState(false);
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+  }
+
+  function handleIsEditClick() {
+    setIsEdit(!isEdit);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    onUpdateUser({
+      name,
+      email,
+    })
+  }
+
+  useEffect(() => {
+    setName(currentUser.name);
+    setEmail(currentUser.email);
+  }, [currentUser]);
+
   return (
     <main className='profile'>
       <h1 className='profile__title'>
-        Привет, Алексей!
+        Привет, {name}!
       </h1>
-      <form className='profile__form'>
+      <form className='profile__form' onSubmit={handleSubmit}>
         <div className='profile__input-container'>
           <label htmlFor='name-input' className='profile__label'>Имя</label>
           <input
@@ -15,8 +48,12 @@ export default function Profile() {
             id='name-input'
             required
             className='profile__input'
-            minLength={2}
-            maxLength={30}
+            minLength="2"
+            maxLength="30"
+            value={name}
+            onChange={handleNameChange}
+            placeholder="Имя"
+            disabled={isEdit ? false : true}
           />
         </div>
         <div className='profile__input-container'>
@@ -27,11 +64,15 @@ export default function Profile() {
             id='email-input'
             required
             className='profile__input'
+            value={email}
+            onChange={handleEmailChange}
+            placeholder="Email"
+            disabled={isEdit ? false : true}
           />
         </div>
-        <button className='profile__btn' type='submit'>Редактировать</button>
+        <button className={`profile__btn ${isEdit ? 'profile__btn__hidden' : ''}`} type='button' onClick={handleIsEditClick}>Редактировать</button>
       </form>
-      <button className='profile__btn profile__btn_color_pink'>Выйти из аккаунта</button>
+      <button className={`profile__btn profile__btn_color_pink ${isEdit ? 'profile__btn__hidden' : ''}`}>Выйти из аккаунта</button>
     </main>
   )
 }
