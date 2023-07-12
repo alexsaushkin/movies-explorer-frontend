df42d64805205f94761a205764870bd253d25a86
alexsaushkin <thirdyou@yandex.ru>
2023-07-12 18:02:43 +0300

    add: start movies func

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,18 +1,31 @@
-import {useState} from "react";
+import {useCallback, useState} from "react";
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
 import './Movies.css';
 
-export default function Movies({ savedMovies, onSearch, onSave, onDelete, isLoading }) {
-  const [ allMovies, setAllMovies ] = useState([]);
+export default function Movies({savedMovies, onSearch, onSave, onDelete, isLoading}) {
+  // все фильмы
+  const [movies, setMovies] = useState([]);
+  // чекбокс миниатюр
   const [isCheckOn, setIsCheckOn] = useState(false);
+
+  function handleMovieSearch(movies, search, isSaved) {
+    // убираем лишнее из поискового запроса
+    const searchTrimmed = search.toLowerCase().trim();
+    // фильтруем фильмы по запросу
+    const moviesFiltered = movies.filter((movie) => {
+      return movie.nameRU.toLowerCase().trim().includes(searchTrimmed) ||
+        movie.nameEN.toLowerCase().trim().includes(searchTrimmed);
+    });
+    return moviesFiltered;
+  }
 
   return (
     <main className='movies'>
-      <SearchForm />
+      <SearchForm/>
       <MoviesCardList
-        movies={allMovies}
+        movies={movies}
         savedMovies={savedMovies}
         isSaved={false}
         onSave={onSave}
